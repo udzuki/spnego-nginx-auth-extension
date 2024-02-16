786a48c8126e093b6d723523ca1329f67025c9ef
mnagaku <mnagaku@gmail.com>
2024-02-16 20:42:27 +0900

    最長一致かつ前方一致でエントリを取得

--- auth.py
+++ auth.py
@@ -44,12 +44,16 @@ def extract_url(request_uri):
 
 
 def get_entry(url, port):
+    ret_entry = None
     if port not in acl:
-        return None
+        return ret_entry
+    keylen = 0
     for k, v in acl[port].items():
-        if url.startswith(k):
-            return v
-    return None
+        now_keylen = len(k)
+        if now_keylen > keylen and url.startswith(k):
+            keylen = now_keylen
+            ret_entry = v
+    return ret_entry
 
 
 def get_authorized_usernames(url, port):
